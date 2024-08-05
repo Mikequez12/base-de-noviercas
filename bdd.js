2753d7389dbededf5cc08e622003a7d0ea64d093
Mikequez12 <142720633+Mikequez12@users.noreply.github.com>
2024-08-05 13:22:01 +0200

    Create bdd.js

---
+++ bdd.js
@@ -0,0 +1,8 @@
+bdd = {
+  "Markel Rosco Matxain": {'DSR':"747821BV238AX"},
+  "Ander Ímaz Ucedo": {'DSR':"218216BF109KA"},
+  "Nekane Matxain Irastorza": {'DSR':"447269HS162WH"},
+  "Vika Lesmes Gil": {'DSR':"154134AN225IL"}
+};
+
+export { bdd };
